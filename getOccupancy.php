0cf9e5f6049fce8ffb52ab7efd24516a677f7a67
gibtmirdas <gibtmirdas@hotmail.fr>
2014-05-20 19:54:59 +0530

    asd

--- getOccupancy.php
+++ getOccupancy.php
@@ -17,9 +17,10 @@ mysql_select_db("$db_name" , $con) or die ("could not load the database" . mysql
  * Return occupancy_name *
  *************************/
 $return="";
-$leaderboard = mysql_query("SELECT occupancy_name FROM `".$table_occupancy."` ORDER BY occupancy_name;");
-while($row = mysql_fetch_assoc($leaderboard)){
-	$return.=$row['occupancy_name']."-";
-}
-echo substr($return, 0, strlen($return)-1);
+$leaderboard = mysql_query("SELECT skills_name FROM `".$table_skiskills."` ORDER BY skills_name;");
+// while($row = mysql_fetch_assoc($leaderboard)){
+// 	$return.=$row['occupancy_name']."-";
+// }
+// echo substr($return, 0, strlen($return)-1);
+echo sqlToXml($leaderboard, $table_skiskills."s", $table_skiskills)
 ?>
