baa0bd37d03bfa36b871bd8a37a45e72fa43f4f7
gibtmirdas <gibtmirdas@hotmail.fr>
2014-05-20 17:49:58 +0530

    ydf

--- getMajors.php
+++ getMajors.php
@@ -18,9 +18,9 @@ mysql_select_db("$db_name" , $con) or die ("could not load the database" . mysql
  *********************/
 $return="";
 $leaderboard = mysql_query("SELECT major_name FROM `".$table_major."` ORDER BY major_name;");
-while($row = mysql_fetch_assoc($leaderboard)){
-	$return.=$row['major_name']."-";
-}
+// while($row = mysql_fetch_assoc($leaderboard)){
+// 	$return.=$row['major_name']."-";
+// }
 //echo substr($return, 0, strlen($return)-1);
 $toRet="haha". sqlToXml($leaderboard, "LeaderBoard", "Fuck");
 
