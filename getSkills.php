c03bfd3b454d7f00b8645fc46a38970d887fd4f8
gibtmirdas <gibtmirdas@hotmail.fr>
2014-05-20 19:56:46 +0530

    sadf

---
+++ getSkills.php
@@ -0,0 +1,21 @@
+<?PHP
+
+include 'config.php';
+
+/**************
+ * Connection *
+***************/
+// Try to connect to DB with unity user
+$con = mysql_connect($url,$sql_usr,$sql_pwd) or ("Cannot connect!"  . mysql_error());
+if (!$con)
+	die('Could not connect: ' . mysql_error());
+
+// Select unity DB
+mysql_select_db("$db_name" , $con) or die ("could not load the database" . mysql_error());
+
+/*********************
+ * Return major_name *
+ *********************/
+$leaderboard = mysql_query("SELECT major_name FROM `".$table_skiskills."` ORDER BY major_name;");
+echo sqlToXml($leaderboard, $table_major."s", $table_major);
+?>
