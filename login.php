ee62056733be978defe1a0ca9aabe1f6755e8b77
gibtmirdas <gibtmirdas@hotmail.fr>
2014-06-22 18:22:53 +0530

    asd

--- login.php
+++ login.php
@@ -11,11 +11,27 @@ if (!$con)
 
 mysql_select_db($db_name , $con) or die ("could not load the database" . mysql_error());
 
-$query = mysql_query("SELECT * FROM `".$table_player."` WHERE `username`='".$user."' AND `password`='".$pass."'");
-while($row = mysql_fetch_assoc($query)){
-	if(!empty($row['username']) AND !empty($row['password']))
-		echo txtToXML("login", "OK");
-	else
-		echo txtToXML("login", "3Error: Username or password incorrect");
+// $query = mysql_query("SELECT * FROM `".$table_player."` WHERE `username`='".$user."' AND `password`='".$pass."'");
+// while($row = mysql_fetch_assoc($query)){
+// 	if(!empty($row['username']) AND !empty($row['password']))
+// 		echo txtToXML("login", "OK");
+// 	else
+// 		echo txtToXML("login", "3Error: Username or password incorrect");
+// }
+
+
+
+
+$query = mysql_query("SELECT * FROM `".$table_player."` WHERE `username`='".$user."' AND `password`='".$pass."'") or die(mysql_error());
+$row = mysql_fetch_array($query) or die(mysql_error()); 
+if(!empty($row['username']) AND !empty($row['password'])) { 
+	$_SESSION['username'] = $row['password']; 
+	echo "SUCCESSFULLY LOGIN TO USER PROFILE PAGE..."; 
+} else { 
+	echo "SORRY... YOU ENTERD WRONG ID AND PASSWORD... PLEASE RETRY..."; 
 }
+
+
+
+
 ?>
