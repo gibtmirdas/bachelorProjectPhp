00060fca235ad60a6126d4ad7f34478e39bc1920
gibtmirdas <gibtmirdas@hotmail.fr>
2014-06-22 18:41:18 +0530

    asd

--- login.php
+++ login.php
@@ -1,38 +1,22 @@
 <?PHP
 include 'config.php';
 
-// $user = $_POST['user'];
-// $pass = $_POST['password'];
-$user="thomas";
-$pass="00e29ed2637611b998c5bf577783d8d3a";
+$user = $_POST['user'];
+$pass = $_POST['password'];
+// $user="thomas";
+// $pass="00e29ed2637611b998c5bf577783d8d3a";
 $con = mysql_connect($url,$sql_usr,$sql_pwd) or ("Cannot connect!"  . mysql_error());
 if (!$con)
 	die('Could not connect: ' . mysql_error());
 
 mysql_select_db($db_name , $con) or die ("could not load the database" . mysql_error());
 
-// $query = mysql_query("SELECT * FROM `".$table_player."` WHERE `username`='".$user."' AND `password`='".$pass."'");
-// while($row = mysql_fetch_assoc($query)){
-// 	if(!empty($row['username']) AND !empty($row['password']))
-// 		echo txtToXML("login", "OK");
-// 	else
-// 		echo txtToXML("login", "3Error: Username or password incorrect");
-// }
-
-
-
-
 $query = mysql_query("SELECT * FROM `".$table_player."` WHERE `username`='".$user."' AND `password`='".$pass."'") or die(mysql_error());
-$row = mysql_fetch_array($query) or die(mysql_error()); 
-if($row == "") echo "AAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+$row = mysql_fetch_array($query);
 if(!empty($row['username']) AND !empty($row['password'])) { 
 	$_SESSION['username'] = $row['password']; 
-	echo "SUCCESSFULLY LOGIN TO USER PROFILE PAGE..."; 
+		echo txtToXML("login", "OK");
 } else { 
-	echo "SORRY... YOU ENTERD WRONG ID AND PASSWORD... PLEASE RETRY..."; 
+		echo txtToXML("login", "Error: Username or password incorrect");
 }
-
-
-
-
 ?>
